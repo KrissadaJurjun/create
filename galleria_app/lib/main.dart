3c98ba940de11f08359e5c1827a335ed98129440
KrissadaJurjun <153060949+KrissadaJurjun@users.noreply.github.com>
2025-08-04 13:44:51 +0700

    aersrgs

--- galleria_app/lib/main.dart
+++ galleria_app/lib/main.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const Myapp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Galleria App',
+    theme: HomeScreen(),
+    );
+  };
+}
