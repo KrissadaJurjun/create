34eac94888a3bf30b944f9eaa339e0855f96674d
KrissadaJurjun <153060949+KrissadaJurjun@users.noreply.github.com>
2025-08-04 14:16:59 +0700

    Update Curve Nav bar

---
+++ galleria_app/lib/constant/app_contsant.dart
@@ -0,0 +1,19 @@
+import package:flutter/material.dart';
+
+const Color primaryColor = Colors.blue;
+const Color secondaryColor = Colors.grey;
+
+const Color accdentColor = Colors(OxFF00C853); // Green accent color
+
+TextStyle TopheadingtextStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  color: primaryColor,
+);
+
+TextStyle bodyTextStyle = TextStyle(
+  fontSize: 16,
+  color: secondaryColor,
+);
+
+
